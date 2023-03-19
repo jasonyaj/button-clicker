94c7358033ebfc4258feb9b86c0fba6ee9122d4a
jasonyaj <jason.yaj@gmail.com>
2023-03-18 19:26:34 -0500

    initial commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+#Web Fundamentals-Core-Button Clicker
+
+fix dead space between left and right colums
+
+link google font roboto
---
+++ script.js
@@ -0,0 +1,11 @@
+// remove the "Add Definition" button when clicked
+
+function logInOut(element) {
+  element.innerText = "Logout";
+}
+
+// Change the text to say "Logout" when clicking the "Login" button
+
+function hide(element) {
+  element.remove();
+}
